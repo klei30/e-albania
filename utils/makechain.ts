eed12a93a5ecfb4e444588381b9ca5e7833e7980
klei aliaj <121291603+klei30@users.noreply.github.com>
2023-08-01 00:20:31 +0200

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -5,7 +5,7 @@ import { PromptTemplate } from 'langchain/prompts';
 import { CallbackManager } from 'langchain/callbacks';
 
 const CONDENSE_PROMPT =
-  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, rephrase the follow up question in ALbanian language and its dialects, to be a standalone question.
 
 Chat History:
 {chat_history}
@@ -13,13 +13,12 @@ Follow Up Input: {question}
 Standalone question:`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(
-  `Dua që të veprosh si një asistent qe shkruan shqip për navigimin e shërbimeve të e-Albania.
-  Ti do lexosh nga një dokument PDF në e-Albania dhe do të ofrosh përgjigje strikte të sakta. Nëse nuk e di përgjigjen, thjesht thuaj 
-  se nuk e di, në vend se të shpikësh një. Thekso se je programuar për të përgjigjur pyetjeve që kanë lidhje me 
-  context dhe përgjigju me mirësjellje pyetjeve që nuk kanë lidhje me context. Jepu tonin e miqësor, 
-  i mirësjellshëm dhe shpjegoi gjërat në detaje. Asistoi gjithmonë hap pas hapi në përdorimin 
-  e shërbimeve të e-Albania. Për më tepër, përdor lidhjet për t'u referuar ndaj shërbimeve të ndryshme.
-  Dergo edhe cdo link ose hyprlinks sipas context
+  `Roli juaj është të shërbeni si një asistent ne gjuhen shqipe që lehtëson përdorimin e shërbimeve të e-Albania https://e-albania.al/, duke ofruar
+   përgjigje në gjuhën shqipe. Do të merrni informacion nga PDF-të brenda e-Albania, duke siguruar saktësinë e 
+   përgjigjeve. Thekso se je programuar për të përgjigjur pyetjeve që kanë lidhje me 
+   context dhe përgjigju me mirësjellje pyetjeve që nuk kanë lidhje me context. Jepu tonin e miqësor, 
+   i mirësjellshëm dhe shpjegoi gjërat në detaje. Asistoi gjithmonë hap pas hapi në përdorimin 
+   e shërbimeve të e-Albania. Sigurohuni qe mos te japesh KURR URL ose Links
  {context}
  
  Question: {question}
@@ -31,12 +30,11 @@ export const makeChain = (
   onTokenStream?: (token: string) => void,
 ) => {
   const questionGenerator = new LLMChain({
-    llm: new OpenAIChat({ temperature: 1 }),
+    llm: new OpenAIChat({ temperature: 0.7   }),
     prompt: CONDENSE_PROMPT,
   });
   const docChain = loadQAChain(
     new OpenAIChat({
-      temperature: 0.7,
       modelName: 'gpt-3.5-turbo-16k',//change this to older versions (e.g. gpt-3.5-turbo) or (gpt-4) 
       maxTokens:2000 ,
       streaming: Boolean(onTokenStream),
