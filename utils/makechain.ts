e39096ec4c050d40a56bcd3afeb0a61045c8eff0
klei aliaj <121291603+klei30@users.noreply.github.com>
2023-08-08 09:19:52 +0200

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -31,7 +31,7 @@ export const makeChain = (
   onTokenStream?: (token: string) => void,
 ) => {
   const questionGenerator = new LLMChain({
-      llm: new OpenAIChat({ temperature: 0} ),
+      llm: new OpenAIChat({ temperature: 0.5} ),
       prompt: CONDENSE_PROMPT,
     });
     const docChain = loadQAChain(
