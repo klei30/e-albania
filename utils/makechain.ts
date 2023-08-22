6fda9863177c923741552c20509d50427fa6f7a0
klei aliaj <121291603+klei30@users.noreply.github.com>
2023-08-22 20:23:19 +0200

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -4,25 +4,30 @@ import { PineconeStore } from 'langchain/vectorstores';
 import { PromptTemplate } from 'langchain/prompts';
 import { CallbackManager } from 'langchain/callbacks';
 
-const CONDENSE_PROMPT =
-  PromptTemplate.fromTemplate(`Given the following conversation and a follow up question, respond in Albanian language the follow up question to be a standalone question.DO not produce and DO NOT rephrase new and fake information AND DO NOT SEND LINKS  
+const CONDENSE_PROMPT = PromptTemplate.fromTemplate(`
+Given the following conversation and a follow up question, craft the follow up question so that it is standalone while retaining its exact context.
 
 Chat History:
 {chat_history}
-Follow Up Input: {question}
-Standalone question:`);
+
+Follow Up Input: 
+{question}
+
+Standalone Question:
+`);
 
 const QA_PROMPT = PromptTemplate.fromTemplate(`
-  Ju jeni një sistem që asiston përdoruesit rreth shërbimeve të E-albania në gjuhën shqipe. Roli juaj është të ofroni informacion të saktë, të detajuar, dhe faktik, duke u bazuar në të dhënat e suportuara. 
-  Kur ju pershendesin, pershendeti edhe ti.
-  Ju duhet të ndjekni striktësisht të dhënat e bazuara në dokumentet burimore, dhe çdo devijim do të penalizohet. Veçanërisht për URL-të, gjithmonë duhet të jepni linke të sakta dhe të paprekura nga burimi origjinal. 
-  Përgjigjet tuaja duhet të jenë miqësore dhe të orientuara drejt objektivave dhe rrethanave të përdoruesit, duke suportuar një përvojë të këndshme. 
-  Nëse merrni pyetje që nuk janë të lidhura me E-Albania dhe materialin e suportuar, në mënyrë miqësore refuzoni tu përgjigjeni.
-  Ju jeni të lidhur me parimet e mësipërme dhe çdo shkelje e tyre është e papranueshme.
-  Mos dergo kurr links ose hyperlinks pervec https://e-albania.al/
-  Konteksti: {context}
-  Pyetja: {question}
-  Përgjigjja (në HTML markdown): `);
+Ju jeni një asistent që ndihmon përdoruesit me informacion rreth shërbimeve të E-albania në gjuhën shqipe. 
+Ju duhet të ofroni përgjigje të saktë, të detajuara dhe faktike, duke u bazuar vetëm në dokumentet e përfshira.
+
+Është e rëndësishme të ndjekni striktësisht informacionin nga dokumentet origjinale. Kjo përfshin edhe lidhjet URL: ju duhet gjithmonë të jepni linke të sakta dhe origjinale, dhe MOS të krijoni ose modifikoni lidhje që nuk janë përfshirë në dokumentet origjinale.
+
+Nëse ju pyesin për diçka që nuk është në dokumentet e përfshira, refuzojeni të përgjigjeni me korrektësi. 
+Përgjigjet tuaja duhet të jenë miqësore dhe të orientuara ndaj përdoruesit.
+
+Konteksti: {context}
+Pyetja: {question}
+Përgjigjja (në HTML markdown):`);
 
 
 
@@ -31,13 +36,15 @@ export const makeChain = (
   onTokenStream?: (token: string) => void,
 ) => {
   const questionGenerator = new LLMChain({
-      llm: new OpenAIChat({ temperature: 0.5} ),
+      llm: new OpenAIChat({ temperature: 0.2} ),
       prompt: CONDENSE_PROMPT,
     });
     const docChain = loadQAChain(
       new OpenAIChat({
         modelName: 'gpt-3.5-turbo-16k',//change this to older versions (e.g. gpt-3.5-turbo) or (gpt-4) 
-        maxTokens:4000,
+        maxTokens:6000,
+        topP:0.8,
+        presencePenalty:-0.5,
       streaming: Boolean(onTokenStream),
       callbackManager: onTokenStream
         ? CallbackManager.fromHandlers({
@@ -56,6 +63,6 @@ export const makeChain = (
     combineDocumentsChain: docChain,
     questionGeneratorChain: questionGenerator,
     returnSourceDocuments: true,
-    k: 1, //number of source documents to return
+    k: 5, //number of source documents to return
   });
 };
