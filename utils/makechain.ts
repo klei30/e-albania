840c3b82df4b2b920a6661016e0a8cc36b6f2a41
klei aliaj <121291603+klei30@users.noreply.github.com>
2023-08-28 11:35:23 +0200

    Update makechain.ts

--- utils/makechain.ts
+++ utils/makechain.ts
@@ -5,23 +5,19 @@ import { PromptTemplate } from 'langchain/prompts';
 import { CallbackManager } from 'langchain/callbacks';
 
 const CONDENSE_PROMPT = PromptTemplate.fromTemplate(`
-Your task is to craft a standalone follow-up question in Albanian, based on the provided chat history. Ensure that the new question retains the exact context of the original input and doesn't alter any information.
+Given the following conversation and a follow up question, craft in Albanian language the follow up question to be a standalone question.
 
 Chat History:
 {chat_history}
-
-Original Follow Up Question: 
-{question}
-
-Crafted Standalone Question in Albanian:`);
+Follow Up Input: {question}
+Standalone question:`);
 
 
 const QA_PROMPT = PromptTemplate.fromTemplate(`
 Ju jeni një asistent që ndihmon përdoruesit me informacion rreth shërbimeve të E-albania në gjuhën shqipe. 
 Ju duhet të ofroni përgjigje të saktë, të detajuara dhe faktike, duke u bazuar vetëm në dokumentet e përfshira.
+Është e rëndësishme të ndiqni striktësisht informacionin nga dokumentet origjinale.
 
-Është e rëndësishme të ndjekni striktësisht informacionin nga dokumentet origjinale.
-    
 Nëse ju pyesin për diçka që nuk është në dokumentet e përfshira, refuzojeni të përgjigjeni me korrektësi. 
 Përgjigjet tuaja duhet të jenë miqësore dhe të orientuara ndaj përdoruesit.
 Nese ju pershendesin, pershendeti edhe ti miqesisht
@@ -43,7 +39,7 @@ export const makeChain = (
     const docChain = loadQAChain(
       new OpenAIChat({
         modelName: 'gpt-4',//change this to older versions (e.g. gpt-3.5-turbo) or (gpt-4) 
-        maxTokens:600,
+        maxTokens:1000,
       streaming: Boolean(onTokenStream),
       callbackManager: onTokenStream
         ? CallbackManager.fromHandlers({
